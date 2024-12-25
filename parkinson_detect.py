9caf3236814befee33399148b8b4f90cb57415fd
Sergeyi <follhaus71@gmail.com>
2024-12-25 12:28:55 +0300

    Module import

---
+++ parkinson_detect.py
@@ -0,0 +1,11 @@
+# Import Lib
+import os, sys
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import xgboost as xgb
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
